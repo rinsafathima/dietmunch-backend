3c6add0c2df287703aa30d88ff853821292d1ae9
Dilmi Rangana <dilmirangana1234@gmail.com>
2024-03-23 15:53:58 +0530

    Appointment delete

--- src/main/java/com/dietmunch/repo/AppointmentRpo.java
+++ src/main/java/com/dietmunch/repo/AppointmentRpo.java
@@ -13,4 +13,5 @@ import java.util.List;
 public interface AppointmentRpo extends JpaRepository<Appointments,Integer> {
 
     List<Appointments> getByUserId(int userId);
+    void deleteById(Integer id);
 }
